f7dad1a96cb178251635d3f67c09919d6d5f9144
Forrest Parker <forresta.parker@gmail.com>
2014-11-10 21:54:49 -0800

    double link passes

--- test/dedup_test.js
+++ test/dedup_test.js
@@ -2,12 +2,12 @@ var linkedlist = require("../lib/linked_list.js");
 var assert = require("assert");
 var should = require("should");
 
-var list = new linkedlist.LinkedList();
+var ll = new linkedlist.LinkedList();
 
 describe("Dedupe", function () {
   it("fill a list and then check for duplicates", function() {
-    list.fillRandom(200, 100);
-    list.dedupe();
-    console.log(list);
+    ll.fillRandom(200, 100);
+    ll.dedupe();
+    console.log(ll);
   });
 });
--- test/linked_list_test.js
+++ test/linked_list_test.js
@@ -2,47 +2,43 @@ var linkedlist = require("../lib/linked_list.js");
 var assert = require("assert");
 var should = require("should");
 
-var list = new linkedlist.LinkedList();
+var ll = new linkedlist.LinkedList();
 
 
 describe("DoubleLinkedList", function () {
   it("should return size 0 before inserting anything", function() {
-    should.equal(list.size(), 0);
+    should.equal(ll.size(), 0);
     })
 
   it('search should return null because node is not there', function () {
-    should.equal(list.search('forrest'), null);
+    should.equal(ll.search('forrest'), null);
   })
 
-  it('remove should return null because node is not there', function () {
-    should.equal(list.remove('forrest'), null);
-    })
-
   it('toString should return empty string because list is empty', function () {
-    should.equal(list.toString(), "");
+    should.equal(ll.toString(), "");
     })
 
   it("should return size of array after inserting", function () {
-    list.insert('hi');
-    list.insert('my');
-    list.insert('name');
-    list.insert('is');
-    list.insert('forrest');
+    ll.insert('hi');
+    ll.insert('my');
+    ll.insert('name');
+    ll.insert('is');
+    ll.insert('forrest');
 
-    should.equal(list.size(), 5);
+    should.equal(ll.size(), 5);
   })
 
   it('should return the node whose value we searched for', function () {
-    should.equal('forrest', list.search('forrest').value);
+    should.equal('forrest', ll.search('forrest').value);
   })
 
   it('should remove a node', function () {
-    list.remove('hi');
-    should.equal(null, list.search('hi'));
-    should.equal(list.size(), 4);
+    ll.remove('hi');
+    should.equal(null, ll.search('hi'));
+    should.equal(ll.size(), 4);
   })
 
   it('should return a string', function () {
-    should.equal(list.toString(), "my, name, is, forrest");
+    should.equal(ll.toString(), "my, name, is, forrest");
   })
 })
